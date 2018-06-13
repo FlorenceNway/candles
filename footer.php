43ab189ec50c1b576a482e0865eb909656b929a1
Darragh McAlone <darraghmcalone@hotmail.co.uk>
2018-06-13 17:17:43 +0100

    Displaying navigation bar
    Displaying nav bar in footer for mobile and header for desktop

--- footer.php
+++ footer.php
@@ -13,6 +13,12 @@
 				<div class="site-info">
 					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
 				</div><!-- .site-info -->
+
+				<nav id="mobile-navigation" class="main-navigation" role="navigation">
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?>Menu</button>
+					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+				</nav><!-- #site-navigation -->
+
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
 
