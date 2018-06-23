e2542326615ff7efe67ef3e32c3029a0a8673c51
Darragh McAlone <darraghmcalone@hotmail.co.uk>
2018-06-23 10:59:47 +0100

    Fixed the mixup of nav id and class styling

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
 ?>
 
 			</div><!-- #content -->
-
+			<section id="mobile-block">
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
 					
@@ -18,19 +18,19 @@
 
 					<!-- I added a close button 20/06/18  -->
 					
-			<a href="javascript:void(0)" id="closebtn" onclick="closeNav()">Close</a>
+			
 					
 					<div class="mobile-menu">
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-
+					<a href="javascript:void(0)" id="closebtn" onclick="closeNav()">Close</a>
 
 					</div>
-					<span style="font-size:30px;cursor:pointer" onclick="openNav()">☰ open</span>
-					
+				
 				</nav><!-- #site-navigation -->
 				 <!-- Added a open button 20/06/18-->
-		<span id="openbtn" style="font-size:30px;cursor:pointer" onclick="openNav()">MENU</span>
+		<span id="openbtn" style="font-size:30px;cursor:pointer" onclick="openNav()">MENU</span>	
 			</footer><!-- #colophon -->
+			</section>
 		</div><!-- #page -->
 
 		<!-- added the open and close function for mobile nav -->
