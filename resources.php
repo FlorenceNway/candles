5c448673830ebd4994ae0c8d1e59750e7d121de2
Darragh McAlone <darraghmcalone@hotmail.co.uk>
2018-06-23 21:04:28 +0100

    Created resources page

---
+++ resources.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Template Name: Resources Template
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<div class="resources">
+		<div class="resources-info">	
+		<p>
+		A curate page dedicated to all the really good link out there.
+		<br>
+		<br>
+		Seriously, they're good.	
+		</p>
+		</div>
+		<hr>
+
+		<h2>Design portfolio inspirations</h2>
+		<p>Featuring the best websites and CVs from graphics to motion and UX </p>
+<a href="bestfolios.com">bestfolios.com &#8599;</a>
+		<hr>
+
+		<h2>Colour Contrast Checker</h2>
+		<p>A Colour Contrast Checker with... wait for it, Live Sliders!</p>
+		<a href="webiam.org/resources/contrastchecker">webiam.org/resources/contrastchecker &#8599;</a>
